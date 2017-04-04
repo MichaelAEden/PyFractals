7e2e96b3a77efa7ee5c45c110c3c9a3dced656ed
Michael Eden <Mikey.Eden@gmail.com>
2017-04-04 00:38:56 -0400

    Added custom color schemes, removed pylab, other small changes.

--- fractal.py
+++ fractal.py
@@ -2,14 +2,80 @@ from abc import abstractmethod, ABCMeta
 
 import numpy as np
 import numpy.matlib
+import time
 
-import pylab
+import pylab # TODO: remove with pip
+
+from PIL import Image
 
 class Fractal(object):
     __metaclass__ = ABCMeta
 
     def __init__(self):
         self.name = None
+        self.fractal_map = None
+
+    def generate_fractal(self, n, m, xmin, xmax, ymin, ymax, itermax, seeds):
+        start = time.clock()
+        self.fractal_map = self.generate(n, m, xmin, xmax, ymin, ymax, itermax, seeds)
+        stop = time.clock()
+
+        elapsed_time = stop - start
+
+        print "Time elapsed: " + str(elapsed_time)
+
+    @abstractmethod
+    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax, seeds):
+        """
+        Generates a fractal.
+        :param n: Pixel resolution in x-axis
+        :param m: Pixel resolution in y-axis
+        :param itermax: Maximum number of iterations permitted in each calculation
+        :param xmin: Minimum x-coord (real number line)
+        :param xmax: Maximum x-coord (real number line)
+        :param ymin: Minimum y-coord (imag number line)
+        :param ymax: Maximum y-coord (imag number line)
+        :param seed: Seed for the fractal (unique to each kind of fractal)
+        :return: Image representation of the fractal
+        :rtype: numpy.lib.twodim_base
+        """
+        return None
+
+    def get_image_array(self):
+        fractal_map = self.fractal_map
+        if fractal_map is None:
+            raise Exception("Fractal not generated!")
+
+        color_adjusted_fractal_map = self.adjust_color(fractal_map, (0, 255))
+        zeroes_map = np.zeros(fractal_map.shape)
+
+        # Layers the three arrays into one 3d image
+        data = np.array([color_adjusted_fractal_map, zeroes_map, zeroes_map]).astype(dtype=np.uint8)
+        return data.T
+
+    def display(self):
+        img = Image.fromarray(self.get_image_array(), 'RGB')
+        img.save('temp.png')
+        img.show()
+
+    @staticmethod
+    def adjust_color(image, color_range):
+        """
+        Adjusts color of image so the maximum and minimum values in the array match those in the given color range
+        :param image: Image array
+        :type image: np.ndarray
+        :param color_range: Minimum and maximum color range values
+        :type color_range: tuple
+        :return: Image array adjusted for color
+        """
+
+        color_min = color_range[0]
+        color_range = color_range[1] - color_min
+
+        max_value = np.nanmax(image)
+
+        color_adjusted_image = (image.astype(dtype=float) / max_value * color_range + color_min).astype(dtype=np.uint8)
+        return color_adjusted_image
 
     def get_complex_plane(self, n, m, xmin, xmax, ymin, ymax):
         """Returns a matrix representing the complex plane."""
@@ -25,47 +91,21 @@ class Fractal(object):
 
         return c
 
-    @abstractmethod
-    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax):
-        """
-        Generates a fractal.
-        :param n: Pixel resolution in x-axis
-        :param m: Pixel resolution in y-axis
-        :param itermax: Maximum number of iterations permitted in each calculation
-        :param xmin: Minimum x-coord (real number line)
-        :param xmax: Maximum x-coord (real number line)
-        :param ymin: Minimum y-coord (imag number line)
-        :param ymax: Maximum y-coord (imag number line)
-        :return: Image representation of the fractal
-        :rtype: numpy.lib.twodim_base
-        """
-        return None
-
-    def display(self):
-        fractal = self.generate(500, 500, -1, .5, -1.25, 0.75, 100)
-        pylab.imshow(fractal, origin='lower left')
-        pylab.show()
-
-
 class FractalMandel(Fractal):
-
     def __init__(self):
         Fractal.__init__(self)
         self.name = "Mandelbrot Set"
 
-    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax):
-
+    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax, seed):
         c = self.get_complex_plane(n, m, xmin, xmax, ymin, ymax)
+        z = np.copy(c)
 
         # Generates a fresh matrix with the same dimensions of c which will represent our image
         img = np.matlib.zeros(c.shape, dtype=int)
 
-        # Copy the seed values c into z - this is the matrix which we will actually manipulate
-        z = np.copy(c)
-
         for i in xrange(itermax):  # Like range, except it creates values to iterate through as needed rather than list
             # Mandelbrot function is: f(z) = z^2 + c
-            z *= z
+            z = np.power(z, 2)
             z += c
 
             # If a value exceeds 2.0, it is bound to approach infinity
@@ -75,25 +115,21 @@ class FractalMandel(Fractal):
             # Get all values which are unbounded, and write to them the number of iterations before they became unbounded
             img[unbounded] = i + 1
 
-        # Returns transpose of the image matrix
-        return img.T
+        # Returns the image matrix
+        return img
+
 
 class FractalJulia(Fractal):
-
     def __init__(self):
         Fractal.__init__(self)
         self.name = "Julia Set"
 
-        self.seed = complex(0, 0.64)
-
-    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax):
-
+    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax, seed):
+        c = seed
         z = self.get_complex_plane(n, m, xmin, xmax, ymin, ymax)
 
         # Generates a fresh matrix with the same dimensions of c which will represent our image
         img = np.matlib.zeros(z.shape, dtype=int)
-
-        c = self.seed
 
         for i in xrange(itermax):
             z *= z
@@ -105,52 +141,55 @@ class FractalJulia(Fractal):
             # Get all values which are unbounded, and write to them the number of iterations before they became unbounded
             img[unbounded] = i + 1
 
-        # Returns transpose of the image matrix
-        return img.T
+        # Returns the image matrix
+        return img
+
 
 class FractalNewton(Fractal):
-
     def __init__(self):
         Fractal.__init__(self)
         self.name = "Newton fractal"
 
-        self.seed = complex(0, 0.64)
-
     @staticmethod
-    def f(x):
-        """Seed function of which this will be approximating the root"""
-        return np.power(x, 3) + 1
-
-    @staticmethod
-    def dx(x):
-        """Derivative of the seed function"""
-        return 3 * np.power(x, 2)
-
-    @staticmethod
-    def newtons_iter(f, dx, x):
+    def newtons_iter(f, dx, x, a=1):
         """Approximates the root of the inputted function using one iteration of Newton's method"""
-        return x - (f(x) / dx(x))
+        return x - a * (f(x) / dx(x))
 
-    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax):
-
+    def generate(self, n, m, xmin, xmax, ymin, ymax, itermax, seeds):
+        f = seeds['f']
+        dx = seeds['dx']
+        a = seeds['a']
         z = self.get_complex_plane(n, m, xmin, xmax, ymin, ymax)
 
-        # Identifies which starting points approach a solution in fewer iterations
+        # Tracks the number of iterations required to reach a solution
         root_iters = np.matlib.zeros(z.shape)
 
         # When an approximation comes within this of the root, mark the approximation as a solution
         e = 0.00000001
 
         for i in xrange(itermax):
-            z = self.newtons_iter(self.f, self.dx, z)
+            z = self.newtons_iter(f, dx, z, a)
 
             # Increment the points in the roots_iters matrix where solutions have been found
-            roots = np.where(abs(self.f(z)) < e)
+            roots = np.where(abs(f(z)) < e)
             np.add.at(root_iters, roots, 1.0)
 
-        # Convert the matrix of complex roots into a matrix which can be represented as an image
-        roots = z.real + z.imag
-        img = (25 * roots + 1 * root_iters).astype(dtype=int)
+        solutions = z
 
-        # Returns transpose of the image matrix
-        return img.T
+        # Returns the image matrix
+        return [solutions.real, solutions.imag, root_iters]
+
+    def get_image_array(self):
+        fractal_map = self.fractal_map
+        if fractal_map is None:
+            raise Exception("Fractal not generated!")
+
+        color_adjusted_real_map = self.adjust_color(fractal_map[0], (0, 128))
+        color_adjusted_imag_map = self.adjust_color(fractal_map[1], (64, 128))
+        color_adjusted_iter_map = self.adjust_color(fractal_map[2], (128, 255))
+
+        # Layers the three arrays into one 3d image
+        data = np.array([color_adjusted_real_map,
+                         color_adjusted_imag_map,
+                         color_adjusted_iter_map]).astype(dtype=np.uint8)
+        return data.T
--- fractalgen.py
+++ fractalgen.py
@@ -1,4 +1,38 @@
 from fractal import FractalMandel, FractalJulia, FractalNewton
+import numpy as np
+
+def f_sine(x):
+    """Seed function of which this will be approximating the root"""
+    return np.sin(x)
+
+def dx_sine(x):
+    """Derivative of the seed function"""
+    return np.cos(x)
+
+def f_cosine(x):
+    """Seed function of which this will be approximating the root"""
+    return np.cos(np.sin(x)) - np.pi
+
+def dx_cosine(x):
+    """Derivative of the seed function"""
+    return -1 * np.cos(x) * np.sin(np.sin(x))
+
+def f_polynomial(x):
+    """Seed function of which this will be approximating the root"""
+    return np.power(x, 3) + 1
+
+def dx_polynomial(x):
+    """Derivative of the seed function"""
+    return 3 * np.power(x, 2)
 
 if __name__=='__main__':
-    fractal = FractalNewton().display()
+    np.set_printoptions(threshold=np.nan)
+
+    julia_seed = complex(0, 0.6)
+    mandel_seeds = None
+    newton_seeds = dict(f=f_cosine, dx=dx_cosine, a=1)
+
+    fractal = FractalNewton()
+    fractal.generate_fractal(1000, 1000, -10, 10, -10, 10, 100, newton_seeds)
+    fractal.display()
+
